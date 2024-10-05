4581b8ab5d01752e408f73086b008f9f448176f3
Shrabya35 <shrabyaraj@gmail.com>
2024-10-05 16:16:51 +0545

    cors error

--- server.js
+++ server.js
@@ -9,28 +9,43 @@ import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
 import userRoute from "./routes/userRoute.js";
 import orderRoute from "./routes/orderRoute.js";
-import cors from "cors";
 
 dotenv.config();
 connectDB();
 const app = express();
 
-//middlewares
+// Middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
-const allowedOrigins = [
-  "https://lynxline-client.onrender.com",
-  "http://localhost:3000/",
-];
-app.use(cors({ origin: allowedOrigins }));
 
+// CORS setup
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://lynxline-client.onrender.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
+// Route handlers
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/order", orderRoute);
 
+// Basic routes
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to my app</h1>");
 });
@@ -39,13 +54,14 @@ app.get("/health", (req, res) => {
   res.status(200).json({ message: "Server is healthy!" });
 });
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Something went wrong!" });
 });
 
+// Start server
 const PORT = process.env.PORT || 9080;
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`.bgBlue.white);
 });
